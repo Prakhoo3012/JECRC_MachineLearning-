d73086e5ce45eaf7eed2e02fd43f9b653e92f899
Prakhar kulshrestha <prakharkulshrestha.cse25@jecrc.ac.in>
2022-10-10 14:24:26 +0530

    Gui Updated

--- Movie Recommdation/gui_app.py
+++ Movie Recommdation/gui_app.py
@@ -12,16 +12,19 @@ app = ttk.Tk()
 app.title('My App')
 app.geometry('600x400')
 
+message = ttk.Variable(app)
+
 ttk.Label(app, text = "A Simple Text Label").place(x = 50, y = 50)
-#ttk.Label(app, text = "Prakhar Kulshrestha").place(x = 50, y = 100) 
+ttk.Label(app, textvariable=message).place(x = 50, y = 100) 
 # #configration and placement
 #vigit
 
 def abc() :
     print("Wow")
+    message.set("Jo tera mann kare")
 
-ttk.Button(app, text = "Isko dabba do", command = abc).place(x = 100, y = 100)
-ttk.Button(app, text = "Isko bhi dabba de", command = lambda:print("Hey!! Buddy")).place(x = 100, y =130)
+ttk.Button(app, text = "Isko dabba do", command = abc).place(x = 50, y = 150)
+ttk.Button(app, text = "Isko bhi dabba de", command = lambda:message.set("Pata Nahi")).place(x = 450, y = 150)
 
 #a func can be assigned to an identifer
 #func can be taken as input argument in another func
