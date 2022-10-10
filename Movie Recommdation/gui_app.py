a0bb5169448d5443d4e16b4392b8dc90359f9898
Prakhar kulshrestha <prakharkulshrestha.cse25@jecrc.ac.in>
2022-10-10 14:14:49 +0530

    gui mei update

--- Movie Recommdation/gui_app.py
+++ Movie Recommdation/gui_app.py
@@ -13,7 +13,20 @@ app.title('My App')
 app.geometry('600x400')
 
 ttk.Label(app, text = "A Simple Text Label").place(x = 50, y = 50)
-ttk.Label(app, text = "Prakhar Kulshrestha").place(x = 50, y = 100)
+#ttk.Label(app, text = "Prakhar Kulshrestha").place(x = 50, y = 100) 
+# #configration and placement
+#vigit
+
+def abc() :
+    print("Wow")
+
+ttk.Button(app, text = "Isko dabba do", command = abc).place(x = 100, y = 100)
+ttk.Button(app, text = "Isko bhi dabba de", command = lambda:print("Hey!! Buddy")).place(x = 100, y =130)
+
+#a func can be assigned to an identifer
+#func can be taken as input argument in another func
+#func can be defined under another func
+#a func can be returned from another func
 
 
 
